2f5c489f4013aee5999e88cf3d9fd6d8582b4e53
Ben Silverman <bensilverman10@gmail.com>
2022-04-16 18:10:23 +0100

    Added performance note

--- src/App.tsx
+++ src/App.tsx
@@ -69,6 +69,15 @@ function App() {
           </Text>
           <Space h={"sm"} />
           <Text>
+            <b>Another note:</b> It's likely that this'll run a bit slowly on a lot of people's
+            devices, mainly when the computer is figuring out its next move. Unfortunately, despite
+            the optimisations I've put in place, the algorithm being used is still pretty slow
+            simply due to the sheer number of possible moves that can be made each turn. Don't worry
+            if the application seems to freeze after you've made your move. It should eventually
+            come back to life 😄
+          </Text>
+          <Space h={"sm"} />
+          <Text>
             Have a read of the rules below to familiarise yourself with the game, then hop right in!
           </Text>
           <Title order={3}>Getting Started</Title>
