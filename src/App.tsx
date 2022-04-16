f0b1ab428ab4f8c94afb30e5dd828fd9e9722e11
Ben Silverman <bensilverman10@gmail.com>
2022-04-16 17:03:58 +0100

    Convert SPA to use mantine stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,18 @@
+import { Button, Center, Container, Grid, List, Modal, Text, Title } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
-import styled, { createGlobalStyle } from "styled-components";
 
 import { GameBoard } from "./components/Board";
-import { H1 } from "./components/Heading";
-import { Text } from "./components/Text";
 
 import { Board } from "./utils/board";
 import { Minimax } from "./utils/minimax";
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;  
-    padding: 0;
-  }  
-  
-  html {
-    font-size: 18px;
-  }
-
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
-               Roboto, Oxygen-Sans, Ubuntu, Cantarell,
-               "Helvetica Neue", sans-serif;
-  }
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 960px;
-  margin: 0 auto;
-`;
-
-const Rules = styled.div`
-  width: 100%;
-`;
 
 function App() {
   const [board, setBoard] = useState(new Board());
   const [minimax, setMinimax] = useState(new Minimax());
   const [history, setHistory] = useState<string>(localStorage.getItem("gameHistory") ?? "");
-  const [aiDepth, setAiDepth] = useState(3);
+  const [aiDepth, setAiDepth] = useState(2);
+  const [modalOpened, setModalOpened] = useState(false);
 
   const winner = board.winner.use();
   const numBlackLeft = board.numBlackLeft.use();
@@ -62,8 +32,10 @@ function App() {
   useEffect(() => {
     if (winner === 1) {
       setHistory((prev) => prev + "W");
+      setModalOpened(true);
     } else if (winner === 2) {
       setHistory((prev) => prev + "L");
+      setModalOpened(true);
     }
   }, [winner]);
 
@@ -76,7 +48,7 @@ function App() {
     const difficulty = 3 + 2 * wins - losses;
 
     // Sets AI search depth to between 1 and 6 inclusive.
-    setAiDepth(Math.max(Math.min(6, difficulty), 1));
+    setAiDepth(Math.max(Math.min(4, difficulty), 1));
   }, [history]);
 
   const reset = () => {
@@ -85,21 +57,85 @@ function App() {
   };
 
   return (
-    <Container>
-      <GlobalStyle />
+    <Container fluid>
       <Toaster position="bottom-left" />
-      <H1>Latrunculi</H1>
-      <GameBoard board={board} />
-      {winner === 0 ? null : (
-        <>
-          <H1>We have a winner! Player {winner} wins!</H1>
-          <button onClick={reset}>Reset</button>
-        </>
-      )}
-      <Text># white left: {numWhiteLeft}</Text>
-      <Text># black left: {numBlackLeft}</Text>
-      <Text>History: {history}</Text>
-      <Rules>
+      <Grid>
+        <Grid.Col sm={12} lg={5}>
+          <Button onClick={() => setModalOpened(true)}>Open Modal</Button>
+          <Title order={1}>Latrunculi</Title>
+          <Text>
+            Latrunculi is an ancient Roman board game that has now been brought to the modern world
+            with this online version! Play against the AI opponent to hone your skills and maybe
+            you'll even get good enough to beat it!{" "}
+          </Text>
+
+          <Text># white left: {numWhiteLeft}</Text>
+          <Text># black left: {numBlackLeft}</Text>
+          <Text>History: {history}</Text>
+          <Title order={2}>Rules</Title>
+          <List>
+            <List.Item>
+              The board is oriented with 8 rows of 8 columns, with the player's pawns on the back
+              row and the king on the second row just to the right of the center.
+            </List.Item>
+            <List.Item>White moves first, similar to chess and checkers.</List.Item>
+            <List.Item>
+              Pawns move like rooks in chess. Kings move similarly, but only 1 square at a time.
+            </List.Item>
+            <List.Item>
+              Pawn captures are made by surrounding an opposing piece on two opposing sides (i.e.,
+              in a line) with two of your own pieces; the edge of the board does not count as one of
+              your pieces. The main exception is that captures of a pawn on a corner are made by
+              surrounding the opposing piece on the two exposed sides.
+            </List.Item>
+            <List.Item>
+              Kings cannot be captured, but they can participate in capturing opposing pawns as
+              above.
+            </List.Item>
+            <List.Item>
+              You can move a pawn between two opposing pieces without it being captured.
+            </List.Item>
+            <List.Item>
+              Repeating sequences of moves are not allowed. The same position with the same player
+              to move cannot occur more than three times in the game; after the third occurrence,
+              that position is illegal.
+            </List.Item>
+            <List.Item>
+              A player wins by:
+              <ul>
+                <List.Item>
+                  Immobilizing the opponent's king, even if the opponent's own pieces are blocking
+                  it on some or all sides. This can happen even if the king is not surrounded on all
+                  sides if the open sides are illegal moves due to repetition.
+                </List.Item>
+                <List.Item>Capturing all of the opponent's pawns.</List.Item>
+                <List.Item>
+                  Having more pieces on the board after 50 moves are made with no capture.
+                </List.Item>
+              </ul>
+            </List.Item>
+          </List>
+        </Grid.Col>
+        <Grid.Col sm={12} lg={7}>
+          <Center>
+            <GameBoard board={board} />
+          </Center>
+        </Grid.Col>
+        {winner === 0 ? null : <></>}
+      </Grid>
+
+      <Modal opened={modalOpened} onClose={() => setModalOpened(false)}>
+        <Title order={2}>Player {winner} wins!</Title>
+        <Button
+          onClick={() => {
+            reset();
+            setModalOpened(false);
+          }}>
+          Reset
+        </Button>
+      </Modal>
+
+      {/* <Rules>
         <Text>
           Based on the rules at{" "}
           <a href="http://www.latrunculi.com" target="_blank" rel="noreferrer">
@@ -109,47 +145,47 @@ function App() {
         <details>
           <summary>Read the Rules:</summary>
           <ul>
-            <li>
+            <List.Item>
               The board is oriented with 8 rows of 8 columns, with the player's pawns on the back
               row and the king on the second row just to the right of the center.
-            </li>
-            <li>White moves first, similar to chess and checkers.</li>
-            <li>
+            </List.Item>
+            <List.Item>White moves first, similar to chess and checkers.</List.Item>
+            <List.Item>
               Pawns move like rooks in chess. Kings move similarly, but only 1 square at a time.
-            </li>
-            <li>
+            </List.Item>
+            <List.Item>
               Pawn captures are made by surrounding an opposing piece on two opposing sides (i.e.,
               in a line) with two of your own pieces; the edge of the board does not count as one of
               your pieces. The main exception is that captures of a pawn on a corner are made by
               surrounding the opposing piece on the two exposed sides.
-            </li>
-            <li>
+            </List.Item>
+            <List.Item>
               Kings cannot be captured, but they can participate in capturing opposing pawns as
               above.
-            </li>
-            <li>You can move a pawn between two opposing pieces without it being captured.</li>
-            <li>
+            </List.Item>
+            <List.Item>You can move a pawn between two opposing pieces without it being captured.</List.Item>
+            <List.Item>
               Repeating sequences of moves are not allowed. The same position with the same player
               to move cannot occur more than three times in the game; after the third occurrence,
               that position is illegal.
-            </li>
-            <li>
+            </List.Item>
+            <List.Item>
               A player wins by:
               <ul>
-                <li>
+                <List.Item>
                   Immobilizing the opponent's king, even if the opponent's own pieces are blocking
                   it on some or all sides. This can happen even if the king is not surrounded on all
                   sides if the open sides are illegal moves due to repetition.
-                </li>
-                <li>Capturing all of the opponent's pawns.</li>
-                <li>Having more pieces on the board after 50 moves are made with no capture.</li>
+                </List.Item>
+                <List.Item>Capturing all of the opponent's pawns.</List.Item>
+                <List.Item>Having more pieces on the board after 50 moves are made with no capture.</List.Item>
               </ul>
-            </li>
+            </List.Item>
           </ul>
         </details>
-      </Rules>
-      <p>Debug Board</p>
-      <GameBoard board={minimax.board} />
+      </Rules> */}
+      {/* <p>Debug Board</p>
+      <GameBoard board={minimax.board} /> */}
     </Container>
   );
 }
